16774e6e7001b849fa717a9cb7f20136553c5f7e
Feng Ruohang <fengruohang@outlook.com>
2015-10-08 14:26:56 +0800

    Modified

--- bash/setup_bash.sh
+++ bash/setup_bash.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export CONFIGURATION_HOME=~/usr/Configuration
-rm ~/.bash_inc ~/.bash_profile ~/.bashrc
+export CONFIGURATION_HOME=/home/vonng/usr/Configuration
+rm -f ~/.bash_inc ~/.bash_profile ~/.bashrc
 ln -s $CONFIGURATION_HOME/bash/bash_inc ~/.bash_inc
 ln -s $CONFIGURATION_HOME/bash/bash_profile ~/.bash_profile
 ln -s $CONFIGURATION_HOME/bash/bashrc ~/.bashrc
-source ~/.bashrc
+source /home/vonng/.bashrc
